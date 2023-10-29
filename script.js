0e9956283a300e1200b5b4bee7b0801014b76f8e
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-10-29 15:33:16 +0300

    add classes

--- script.js
+++ script.js
@@ -1,1 +1,135 @@
 "use strict";
+
+class WebProgrammer {
+  constructor(name, surname, age, organization, floor) {
+    name = this.name;
+    surname = this.surname;
+    age = this.age;
+    organization = this.organization;
+    floor = this.floor;
+  }
+}
+
+class FronEndDev extends WebProgrammer {
+  constructor(name, surname, age, organization, floor, lvl = [], skills = []) {
+    super(name, surname, age, organization, floor);
+    lvl = this.lvl;
+    skills = this.skills;
+  }
+}
+
+class JunFrontEndDev extends FronEndDev {
+  constructor(
+    name,
+    surname,
+    age,
+    organization,
+    floor,
+    lvl = [],
+    skills = [],
+    salary,
+    experience
+  ) {
+    super(name, surname, age, organization, floor, (lvl = []), (skills = []));
+    salary = this.salary;
+    experience = this.experience;
+  }
+}
+
+class MidFrontEndDev extends FronEndDev {
+  constructor(
+    name,
+    surname,
+    age,
+    organization,
+    floor,
+    lvl = [],
+    skills = [],
+    salary,
+    experience
+  ) {
+    super(name, surname, age, organization, floor, (lvl = []), (skills = []));
+    salary = this.salary;
+    experience = this.experience;
+  }
+}
+
+class SenFrontEndDev extends FronEndDev {
+  constructor(
+    name,
+    surname,
+    age,
+    organization,
+    floor,
+    lvl = [],
+    skills = [],
+    salary,
+    experience
+  ) {
+    super(name, surname, age, organization, floor, (lvl = []), (skills = []));
+    salary = this.salary;
+    experience = this.experience;
+  }
+}
+
+class BackEndDev extends WebProgrammer {
+  constructor(name, surname, age, organization, floor, lvl = [], skills = []) {
+    super(name, surname, age, organization, floor);
+    lvl = this.skills;
+    skills = this.skills;
+  }
+}
+
+class JunBackEndDev extends BackEndDev {
+  constructor(
+    name,
+    surname,
+    age,
+    organization,
+    floor,
+    lvl = [],
+    skills = [],
+    salary,
+    experience
+  ) {
+    super(name, surname, age, organization, floor, (lvl = []), (skills = []));
+    salary = this.salary;
+    experience = this.experience;
+  }
+}
+
+class MidBackEndDev extends BackEndDev {
+  constructor(
+    name,
+    surname,
+    age,
+    organization,
+    floor,
+    lvl = [],
+    skills = [],
+    salary,
+    experience
+  ) {
+    super(name, surname, age, organization, floor, (lvl = []), (skills = []));
+    salary = this.salary;
+    experience = this.experience;
+  }
+}
+
+class SenBackEndDev extends BackEndDev {
+  constructor(
+    name,
+    surname,
+    age,
+    organization,
+    floor,
+    lvl = [],
+    skills = [],
+    salary,
+    experience
+  ) {
+    super(name, surname, age, organization, floor, (lvl = []), (skills = []));
+    salary = this.salary;
+    experience = this.experience;
+  }
+}
