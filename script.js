a3012d4a46b6c6d8a63ce9e7806c8e753b95bcb6
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-10-31 13:57:01 +0300

    проверка на пустые поля + push значений в массив навыков + таблица

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 "use strict";
+const saveBtn = document.getElementById("save");
+const selectArea = document.getElementById("area");
+const selectLvl = document.getElementById("lvl");
+const nameInput = document.getElementById("name");
+const subnameInput = document.getElementById("subname");
+const ageInput = document.getElementById("age");
+const orgInput = document.getElementById("org");
+const skillsInput = document.getElementById("skills");
+const selectExp = document.getElementById("exp");
+const selectFloor = document.getElementById("floor");
+const child = document.getElementById("children");
 
 class WebProgrammer {
   constructor(name, surname, age, organization, floor) {
@@ -42,6 +53,12 @@ class JunFrontEndDev extends FrontEndDev {
     this.salary = salary;
     this.experience = experience;
   }
+  pushSkills() {
+    let newSkills = skillsInput.value.split(",");
+    newSkills.forEach((item) => {
+      this.skills.push(item);
+    });
+  }
 }
 
 class MidFrontEndDev extends FrontEndDev {
@@ -60,6 +77,12 @@ class MidFrontEndDev extends FrontEndDev {
     this.salary = salary;
     this.experience = experience;
   }
+  pushSkills() {
+    let newSkills = skillsInput.value.split(",");
+    newSkills.forEach((item) => {
+      this.skills.push(item);
+    });
+  }
 }
 
 class SenFrontEndDev extends FrontEndDev {
@@ -77,6 +100,12 @@ class SenFrontEndDev extends FrontEndDev {
     super(name, surname, age, organization, floor, lvl, (skills = []));
     this.salary = salary;
     this.experience = experience;
+  }
+  pushSkills() {
+    let newSkills = skillsInput.value.split(",");
+    newSkills.forEach((item) => {
+      this.skills.push(item);
+    });
   }
 }
 
@@ -104,6 +133,12 @@ class JunBackEndDev extends BackEndDev {
     this.salary = salary;
     this.experience = experience;
   }
+  pushSkills() {
+    let newSkills = skillsInput.value.split(",");
+    newSkills.forEach((item) => {
+      this.skills.push(item);
+    });
+  }
 }
 
 class MidBackEndDev extends BackEndDev {
@@ -121,6 +156,13 @@ class MidBackEndDev extends BackEndDev {
     super(name, surname, age, organization, floor, lvl, (skills = []));
     this.salary = salary;
     this.experience = experience;
+  }
+  pushSkills() {
+    let newSkills = skillsInput.value.split(",");
+    newSkills.forEach((item) => {
+      this.skills.push(item);
+    });
+    console.log(this.skills);
   }
 }
 
@@ -140,6 +182,13 @@ class SenBackEndDev extends BackEndDev {
     this.salary = salary;
     this.experience = experience;
   }
+  pushSkills() {
+    let newSkills = skillsInput.value.split(",");
+    newSkills.forEach((item) => {
+      this.skills.push(item);
+    });
+    console.log(this.skills);
+  }
 }
 
 const addOptions = function () {
@@ -148,8 +197,47 @@ const addOptions = function () {
 
   areaSelect.onchange = function () {
     addOptions.style.cssText = "display: block";
-    console.log(addOptions);
   };
 };
 
+const ifEmpty = function () {
+  const AllInputs = document.querySelectorAll("input[type=text]");
+  let count;
+
+  saveBtn.addEventListener("click", () => {
+    AllInputs.forEach((item) => {
+      if (item.value !== "") {
+        count = 0;
+      } else if (item.value === "") {
+        count = 1;
+      }
+    });
+
+    if (count === 0) {
+      addNewElem();
+    } else if (count === 1) {
+      alert("Заполните поля!");
+    }
+  });
+};
+
+const addNewElem = function () {
+  if (selectArea.value === "front" && selectLvl.value === "jun") {
+    const newElem = new JunFrontEndDev();
+    newElem.pushSkills();
+    console.log(newElem);
+  } else if (selectArea.value === "front" && selectLvl.value === "middle") {
+    const newElem = new MidFrontEndDev();
+    newElem.pushSkills();
+    console.log(newElem);
+  } else if (selectArea.value === "front" && selectLvl.value === "senior") {
+    const newElem = new SenFrontEndDev();
+    newElem.pushSkills();
+    console.log(newElem);
+  } else {
+    console.log("ошибка");
+  }
+};
+
 addOptions();
+ifEmpty();
