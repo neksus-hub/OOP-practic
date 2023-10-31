5e1906e49f090c6f06ec26a1e3a2ebfc8bbe08f1
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-10-31 21:59:16 +0300

    add push object in array

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ const selectFloor = document.getElementById("floor");
 const salaryInput = document.getElementById("salary");
 const child = document.getElementById("children");
 
+const objectData = [];
+
 class WebProgrammer {
   constructor(name, surname, age, organization, floor) {
     this.name = name;
@@ -23,22 +25,6 @@ class WebProgrammer {
 }
 
 class FrontEndDev extends WebProgrammer {
-  constructor(name, surname, age, organization, floor, lvl, skills = []) {
-    super(name, surname, age, organization, floor);
-    this.skills = skills;
-    this.lvl = lvl;
-  }
-}
-
-class BackEndDev extends WebProgrammer {
-  constructor(name, surname, age, organization, floor, lvl, skills = []) {
-    super(name, surname, age, organization, floor);
-    this.skills = skills;
-    this.lvl = lvl;
-  }
-}
-
-class JunFrontEndDev extends FrontEndDev {
   constructor(
     name,
     surname,
@@ -51,11 +37,14 @@ class JunFrontEndDev extends FrontEndDev {
     experience,
     child
   ) {
-    super(name, surname, age, organization, floor, lvl, skills);
+    super(name, surname, age, organization, floor);
+    this.skills = skills;
+    this.lvl = lvl;
     this.salary = salary;
     this.experience = experience;
     this.child = child;
   }
+
   pushSkills() {
     let newSkills = skillsInput.value.split(",");
     newSkills.forEach((item) => {
@@ -79,8 +68,8 @@ class JunFrontEndDev extends FrontEndDev {
         this.name = nameInput.value;
         newTd.innerHTML = `<p>${this.name}</p>`;
       } else if (newTd.id === "1") {
-        this.subname = subnameInput.value;
-        newTd.innerHTML = `<p>${this.subname}</p>`;
+        this.surname = subnameInput.value;
+        newTd.innerHTML = `<p>${this.surname}</p>`;
       } else if (newTd.id === "2") {
         this.age = ageInput.value;
         newTd.innerHTML = `<p>${this.age}</p>`;
@@ -118,6 +107,7 @@ class JunFrontEndDev extends FrontEndDev {
       }
     }
     this.deleteRow();
+    this.pushObjectData();
   }
 
   deleteRow() {
@@ -136,114 +126,27 @@ class JunFrontEndDev extends FrontEndDev {
       });
     }
   }
-}
 
-class MidFrontEndDev extends FrontEndDev {
-  constructor(
-    name,
-    surname,
-    age,
-    organization,
-    floor,
-    lvl,
-    skills,
-    salary,
-    experience,
-    child
-  ) {
-    super(name, surname, age, organization, floor, lvl, skills);
-    this.salary = salary;
-    this.experience = experience;
-    this.child = child;
-  }
-  pushSkills() {
-    let newSkills = skillsInput.value.split(",");
-    newSkills.forEach((item) => {
-      this.skills.push(item);
-    });
+  pushObjectData() {
+    const NewObjectData = {
+      name: this.name,
+      surname: this.surname,
+      age: this.age,
+      organization: this.organization,
+      floor: this.floor,
+      lvl: this.lvl,
+      skills: this.skills,
+      salary: this.salary,
+      experience: this.experience,
+      child: this.child,
+    };
+
+    objectData.push(NewObjectData);
+    console.log(objectData);
   }
 }
 
-class SenFrontEndDev extends FrontEndDev {
-  constructor(
-    name,
-    surname,
-    age,
-    organization,
-    floor,
-    lvl,
-    skills,
-    salary,
-    experience,
-    child
-  ) {
-    super(name, surname, age, organization, floor, lvl, skills);
-    this.salary = salary;
-    this.experience = experience;
-    this.child = child;
-  }
-  pushSkills() {
-    let newSkills = skillsInput.value.split(",");
-    newSkills.forEach((item) => {
-      this.skills.push(item);
-    });
-  }
-}
-
-class JunBackEndDev extends BackEndDev {
-  constructor(
-    name,
-    surname,
-    age,
-    organization,
-    floor,
-    lvl,
-    skills,
-    salary,
-    experience,
-    child
-  ) {
-    super(name, surname, age, organization, floor, lvl, skills);
-    this.salary = salary;
-    this.experience = experience;
-    this.child = child;
-  }
-  pushSkills() {
-    let newSkills = skillsInput.value.split(",");
-    newSkills.forEach((item) => {
-      this.skills.push(item);
-    });
-  }
-}
-
-class MidBackEndDev extends BackEndDev {
-  constructor(
-    name,
-    surname,
-    age,
-    organization,
-    floor,
-    lvl,
-    skills,
-    salary,
-    experience,
-    child
-  ) {
-    super(name, surname, age, organization, floor, lvl, skills);
-    this.salary = salary;
-    this.experience = experience;
-    this.child = child;
-  }
-  pushSkills() {
-    let newSkills = skillsInput.value.split(",");
-    newSkills.forEach((item) => {
-      this.skills.push(item);
-    });
-    console.log(this.skills);
-  }
-}
-
-class SenBackEndDev extends BackEndDev {
+class BackEndDev extends WebProgrammer {
   constructor(
     name,
     surname,
@@ -256,17 +159,112 @@ class SenBackEndDev extends BackEndDev {
     experience,
     child
   ) {
-    super(name, surname, age, organization, floor, lvl, skills);
+    super(name, surname, age, organization, floor);
+    this.skills = skills;
+    this.lvl = lvl;
     this.salary = salary;
     this.experience = experience;
     this.child = child;
   }
+
   pushSkills() {
     let newSkills = skillsInput.value.split(",");
     newSkills.forEach((item) => {
       this.skills.push(item);
     });
-    console.log(this.skills);
+  }
+
+  addNewRow() {
+    const table = document.querySelector(".table");
+
+    const newRow = document.createElement("tr");
+    newRow.classList.add("addrow");
+    table.appendChild(newRow);
+
+    for (let i = 0; i <= 10; i++) {
+      const newTd = document.createElement("td");
+      newTd.classList.add("column");
+      newTd.id = i;
+      newRow.appendChild(newTd);
+      if (newTd.id === "0") {
+        this.name = nameInput.value;
+        newTd.innerHTML = `<p>${this.name}</p>`;
+      } else if (newTd.id === "1") {
+        this.surname = subnameInput.value;
+        newTd.innerHTML = `<p>${this.surname}</p>`;
+      } else if (newTd.id === "2") {
+        this.age = ageInput.value;
+        newTd.innerHTML = `<p>${this.age}</p>`;
+      } else if (newTd.id === "3") {
+        this.organization = orgInput.value;
+        newTd.innerHTML = `<p>${this.organization}</p>`;
+      } else if (newTd.id === "4") {
+        newTd.innerHTML = `<p>${this.skills}</p>`;
+      } else if (newTd.id === "5" && selectFloor.value === "male") {
+        this.floor = "мужской";
+        newTd.innerHTML = `<p>${this.floor}</p>`;
+      } else if (newTd.id === "5" && selectFloor.value === "female") {
+        this.floor = "женский";
+        newTd.innerHTML = `<p>${this.floor}</p>`;
+      } else if (newTd.id === "6" && child.checked) {
+        this.child = "есть";
+        newTd.innerHTML = `<p>${this.child}</p>`;
+      } else if (newTd.id === "6" && !child.checked) {
+        this.child = "нет";
+        newTd.innerHTML = `<p>${this.child}</p>`;
+      } else if (newTd.id === "7") {
+        this.lvl = selectLvl.value;
+        newTd.innerHTML = `<p>${this.lvl}</p>`;
+      } else if (newTd.id === "8") {
+        this.experience = selectExp.value;
+        newTd.innerHTML = `<p>${this.experience}</p>`;
+      } else if (newTd.id === "9") {
+        this.salary = salaryInput.value;
+        newTd.innerHTML = `<p>${this.salary}</p>`;
+      } else if (newTd.id === "10") {
+        const delBtn = document.createElement("button");
+        delBtn.classList.add("delete-button");
+        delBtn.textContent = "Удалить";
+        newTd.appendChild(delBtn);
+      }
+    }
+    this.pushObjectData();
+    this.deleteRow();
+  }
+
+  deleteRow() {
+    const alldelBtn = document.querySelectorAll(".delete-button");
+    const alladdrow = document.querySelectorAll(".addrow");
+
+    console.log(alldelBtn);
+
+    for (let i = 0; i <= alldelBtn.length - 1; i++) {
+      alldelBtn[i].addEventListener("click", () => {
+        for (let j = 0; j <= alladdrow.length - 1; j++) {
+          if (i === j) {
+            alladdrow[j].remove();
+          }
+        }
+      });
+    }
+  }
+
+  pushObjectData() {
+    const NewObjectData = {
+      name: this.name,
+      surname: this.surname,
+      age: this.age,
+      organization: this.organization,
+      floor: this.floor,
+      lvl: this.lvl,
+      skills: this.skills,
+      salary: this.salary,
+      experience: this.experience,
+      child: this.child,
+    };
+
+    objectData.push(NewObjectData);
+    console.log(objectData);
   }
 }
 
@@ -283,7 +281,8 @@ const ifEmpty = function () {
   const AllInputs = document.querySelectorAll("input[type=text]");
   let count;
 
-  saveBtn.addEventListener("click", () => {
+  saveBtn.addEventListener("click", (event) => {
+    event.preventDefault();
     AllInputs.forEach((item) => {
       if (item.value !== "") {
         count = 0;
@@ -301,35 +300,17 @@ const ifEmpty = function () {
 };
 
 const addNewElem = function () {
-  if (selectArea.value === "front" && selectLvl.value === "jun") {
-    const newElem = new JunFrontEndDev();
-    newElem.pushSkills();
-    newElem.addNewRow();
-    console.log(newElem);
-  } else if (selectArea.value === "front" && selectLvl.value === "middle") {
-    const newElem = new MidFrontEndDev();
-    newElem.pushSkills();
-    console.log(newElem);
-  } else if (selectArea.value === "front" && selectLvl.value === "senior") {
-    const newElem = new SenFrontEndDev();
-    newElem.pushSkills();
-    console.log(newElem);
-  } else if (selectArea.value === "back" && selectLvl.value === "jun") {
-    const newElem = new JunBackEndDev();
-    newElem.pushSkills();
-    console.log(newElem);
-  } else if (selectArea.value === "back" && selectLvl.value === "middle") {
-    const newElem = new MidBackEndDev();
-    newElem.pushSkills();
-    console.log(newElem);
-  } else if (selectArea.value === "back" && selectLvl.value === "senior") {
-    const newElem = new SenBackEndDev();
-    newElem.pushSkills();
-    console.log(newElem);
-  } else {
-    console.log("ошибка");
+  if (selectArea.value === "front") {
+    const frontElem = new FrontEndDev();
+    frontElem.pushSkills();
+    frontElem.addNewRow();
+    console.log(frontElem);
+  } else if (selectArea.value === "back") {
+    const backElem = new BackEndDev();
+    backElem.pushSkills();
+    backElem.addNewRow();
+    console.log(backElem);
   }
 };
 
-addOptions();
 ifEmpty();
